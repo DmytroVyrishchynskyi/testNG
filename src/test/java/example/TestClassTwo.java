a7546642f5ce090bc72e235ee84902ca30358d0a
DmytroVyrishchynskyi <magic.scholz@gmail.com>
2024-11-25 17:17:43 +0100

    testng

--- src/test/java/example/TestClassTwo.java
+++ src/test/java/example/TestClassTwo.java
@@ -1,11 +1,12 @@
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestClassTwo {
+
     @Test
     @Parameters({"param1", "param2"})
-    public void testWithParametersFromXML(String param1, String param2) {
+    public void testWithParametersFromXML(@Optional("default1") String param1, @Optional("default2") String param2) {
         System.out.println("Param1: " + param1 + ", Param2: " + param2);
     }
 
@@ -14,11 +15,11 @@ public class TestClassTwo {
         System.out.println("Data1: " + data1 + ", Data2: " + data2);
     }
 
-    @DataProvider
+    @org.testng.annotations.DataProvider
     public Object[][] dataProviderMethod() {
         return new Object[][] {
                 {"DataA", 1},
-                {"DataB", 2}
+                {"DataB", 2},
         };
     }
 }
